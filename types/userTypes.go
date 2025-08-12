05e9dfdb82d41eb083e7b5935b341ab25865456b
Cetin Erdem <cetoerdem1@gmail.com>
2025-08-13 00:26:12 +0400

    add json tags

--- types/userTypes.go
+++ types/userTypes.go
@@ -3,27 +3,27 @@ package types
 import "time"
 
 type User struct {
-	ID           string
-	Name         string
-	Email        string
-	Password     string
-	IsAdmin      bool
-	IsPaid       bool
-	Subscription SubscriptionType
-	RegisterDate time.Time
-	LastLogin    time.Time
-	//TODO: Add Portfolio slice and radar slice which we will take from a stocks api
-	//have to create types according to fetched data
+	ID             string           `json:"id"`
+	Name           string           `json:"name"`
+	Email          string           `json:"email"`
+	PasswordHashed string           `json:"-"`
+	Subscription   SubscriptionType `json:"subscription"`
+	RegisterDate   time.Time        `json:"register_date"`
+	LastLogin      time.Time        `json:"last_login"`
+	IsAdmin        bool             `json:"is_admin"`
+	IsPaid         bool             `json:"is_paid"`
+	// TODO: Add Portfolio slice and radar slice which we will take from a stocks API
+	// have to create types according to fetched data
 }
 
-type RegisterUser struct {
+type CreateUser struct {
 	Email    string `json:"email"`
-	Password string `json:"passoword"`
+	Password string `json:"password"`
 }
 
 type LoginUser struct {
 	Email    string `json:"email"`
-	Password string `json:"passoword"`
+	Password string `json:"password"`
 }
 
 type LoginUserResponse struct {
