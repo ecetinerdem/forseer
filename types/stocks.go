c0e77e1b59d8798b8aa870f7c6ce9bcd98915f8f
Cetin Erdem <cetoerdem1@gmail.com>
2025-08-20 00:32:19 +0400

    create alphadvantage types and stock struct

---
+++ types/stocks.go
@@ -0,0 +1,31 @@
+package types
+
+type AlphaVentageStockResponse struct {
+	MetaData   MetaData               `json:"Meta Data"`
+	TimeSeries map[string]MonthlyData `json:"Monthly Time Series"`
+}
+
+type MetaData struct {
+	Information   string `json:"1. Information"`
+	Symbol        string `json:"2. Symbol"`
+	LastRefreshed string `json:"3. Last Refreshed"`
+	TimeZone      string `json:"4. Time Zone"`
+}
+
+type MonthlyData struct {
+	Open   string `json:"1. open"`
+	High   string `json:"2. high"`
+	Low    string `json:"3. low"`
+	Close  string `json:"4. close"`
+	Volume string `json:"5. volume"`
+}
+
+type Stock struct {
+	Symbol string `json:"symbol"`
+	Month  string `json:"month"`
+	Open   string `json:"open"`
+	High   string `json:"high"`
+	Low    string `json:"low"`
+	Close  string `json:"close"`
+	Volume string `json:"volume"`
+}
