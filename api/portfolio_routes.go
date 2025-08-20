a6902e7f7ff6211919cbbbfc4bc929abc0215e0a
Cetin Erdem <cetoerdem1@gmail.com>
2025-08-20 11:35:16 +0400

    HandleGetPortfolio Route

--- api/portfolio_routes.go
+++ api/portfolio_routes.go
@@ -1,1 +1,36 @@
 package api
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (s *Server) HandleGetPortfolio(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	portfolio, err := s.db.GetStocks(ctx)
+
+	if err != nil {
+		http.Error(w, "Could not get portfolio", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	if err := json.NewEncoder(w).Encode(&portfolio); err != nil {
+		http.Error(w, "Could not encode portfolio", http.StatusInternalServerError)
+	}
+}
+
+func (s *Server) HandleGetStockById(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (s *Server) HandleDeleteStockById(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (s *Server) HandleGetStockBySymbol(w http.ResponseWriter, r *http.Request) {
+
+}
